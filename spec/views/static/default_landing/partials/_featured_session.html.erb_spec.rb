0b0810a4fd611dda03af1013fbc3ebb182e40b3f
benhayehudi <bengreenberg@gmail.com>
2019-03-07 13:00:16 +0200

    stub sessions for featured_session spec test

--- spec/views/static/default_landing/partials/_featured_session.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_featured_session.html.erb_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'rendering _featured_session landing page partial' do
         { title: 'Session 2', description: 'Session Description', video_url: 'https://a/video/path', author: 'An Author', published: true, created_at: Time.zone.today },
       ]
     )
+    allow(@sessions).to receive(:any?).and_return(@sessions)
     render partial: '/static/default_landing/partials/featured_session.html.erb'
 
     expect(rendered).to include('Session 1').or(include('Session 2'))
