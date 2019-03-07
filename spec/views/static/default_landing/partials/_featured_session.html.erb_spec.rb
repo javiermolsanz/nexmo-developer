2f23d6cd56694c95961bfa81060b5f15ef08ad61
benhayehudi <bengreenberg@gmail.com>
2019-03-07 13:09:54 +0200

    passing spec sessions variable to partial during test

--- spec/views/static/default_landing/partials/_featured_session.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_featured_session.html.erb_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'rendering _featured_session landing page partial' do
   it 'renders correctly and alternates between videos' do
-    @sessions = Session.create(
+    sessions = Session.create(
       [
         { title: 'Session 1', description: 'Session Description', video_url: 'https://a/video/path', author: 'An Author', published: true, created_at: Time.zone.today },
         { title: 'Session 2', description: 'Session Description', video_url: 'https://a/video/path', author: 'An Author', published: true, created_at: Time.zone.today },
       ]
     )
-    allow(@sessions).to receive(:any?).and_return(@sessions)
-    render partial: '/static/default_landing/partials/featured_session.html.erb'
+    render partial: '/static/default_landing/partials/featured_session.html.erb', locals: { @sessions => sessions }
 
     expect(rendered).to include('Session 1').or(include('Session 2'))
   end
