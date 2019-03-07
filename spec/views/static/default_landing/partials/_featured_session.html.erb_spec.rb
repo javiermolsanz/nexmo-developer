0373705758eafbfb1bf30afe2629a3b61ecf7758
benhayehudi <bengreenberg@gmail.com>
2019-03-07 09:21:07 +0200

    added first test for featured_session partial

---
+++ spec/views/static/default_landing/partials/_featured_session.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'rendering _featured_session landing page partial' do
+  it 'renders correctly and alternates between videos' do
+    @sessions = Session.create(
+      [
+        { title: 'Session 1', description: 'Session Description', video_url: 'https://a/video/path', author: 'An Author', published: true, created_at: Time.zone.today },
+        { title: 'Session 2', description: 'Session Description', video_url: 'https://a/video/path', author: 'An Author', published: true, created_at: Time.zone.today },
+      ]
+    )
+    render partial: '/static/default_landing/partials/featured_session.html.erb'
+
+    expect(rendered).to include('Session 1').or(include('Session 2'))
+  end
+end
