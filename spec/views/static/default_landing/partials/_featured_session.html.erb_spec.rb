77523e6ec6db5ebd504d9e756a7ed895f57fa89c
benhayehudi <bengreenberg@gmail.com>
2019-03-08 12:42:25 +0200

    removed unneeded variable assignment in featured_video and refactored spec to use local_assigns

--- spec/views/static/default_landing/partials/_featured_session.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_featured_session.html.erb_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'rendering _featured_session landing page partial' do
   it 'renders correctly' do
-    render partial: '/static/default_landing/partials/featured_session.html.erb'
+    render partial: '/static/default_landing/partials/featured_session.html.erb', locals: {
+      'featured' => { 'title' => 'Featured Session Title 1', 'video_url' => 'http://a/path/to/a/video' },
+    }
 
     expect(rendered).to include('<iframe src=')
   end
